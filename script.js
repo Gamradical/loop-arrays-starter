2f689a90ff78ecd52e7395efb9028aaaac5a5f7e
Maureen_g <gamradicalsix@gmail.com>
2023-03-23 13:14:58 -0500

    Initial commit, working with arrays and loops

---
+++ script.js
@@ -0,0 +1,30 @@
+var programmingLanguages = [
+  "Scala",
+  "Java",
+  "Go",
+  "Swift",
+  "Cobol",
+  "JavaScript",
+  "Python",
+  "Ruby",
+  "Elm",
+  "Rust",
+  "C#"
+];
+
+//Output Strings with a for...of Loop
+console.log(programmingLanguages);
+for (var language of programmingLanguages) {
+  console.log(`I want to learn ${language}!`);
+}
+
+//Create a Numbered List with forEach()
+programmingLanguages.forEach(function (language, index) {
+  console.log(`${index + 1}.  ${language.toUpperCase()}`);
+});
+
+//Filter Elements by a String
+var updatedLanguages = programmingLanguages.filter(function (language) {
+  return language.includes("y");
+});
+console.log(updatedLanguages);
